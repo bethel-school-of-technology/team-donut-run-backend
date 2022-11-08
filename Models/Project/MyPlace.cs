62d7f7bc62b948414a7cc88f848391a862d559e1
Hope Ross <hope.ross@daveramsey.com>
2022-11-07 18:53:12 -0600

    redid place related models

--- Models/Project/MyPlace.cs
+++ Models/Project/MyPlace.cs
@@ -5,8 +5,6 @@ namespace rexfinder_api.Models;
 
 public class MyPlace
 {
-
-    //these items are set by our app
     [JsonIgnore]
     public int MyPlaceId { get; set; }
     public bool Visited { get; set; }
@@ -16,13 +14,6 @@ public class MyPlace
     [Required]
     public string CreatedOn { get; set; }
 
-    //below items are a local copy of the data from the google places API, a local cache if you will.  We should pull the most current data from google places API but having a local cache helps for speed and if google places API doesn't respond
     [Required]
     public string GooglePlaceId { get; set; }
-
-    public string PlaceImage { get; set; }
-    public string PlaceName { get; set; }
-    public string City { get; set; }
-    public string State { get; set; }
-    public string StarRating { get; set; }
 }
