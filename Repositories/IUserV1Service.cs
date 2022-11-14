9ea1067770f28c76744eb7f68cf541e9f998d9a8
Keith Isbell <keith.isbell@Keith-Isbell-Lenovo>
2022-11-13 16:48:19 -0800

    Added User Repositories

---
+++ Repositories/IUserV1Service.cs
@@ -0,0 +1,11 @@
+using rexfinder_api.Models;
+
+namespace rexfinder_api.Repositories;
+
+public interface IUserV1Service {
+
+    IEnumerable<UserV1> GetAllUsers();
+    UserV1? GetUserById(int userId);
+    UserV1? UpdateUser(UserV1 newUser);
+    void DeleteUserById(int userId);
+}
